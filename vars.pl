68a42c32e18f47f171ce3393a86b0cbf234ee924
Jacob Pachev <jacobpachev@gmail.com>
2016-01-08 20:04:17 -0700

    Learned more about variables.

--- vars.pl
+++ vars.pl
@@ -3,8 +3,9 @@
 use strict;
 use warnings;
 
-my $i = 1; 
-print "i=$i\n";
-
-
-
+my $i = 1;
+print "at first i=$i\n";
+$i++;
+print "after increment i=$i\n";
+$i = 3;
+print "after assignment i=$i\n";

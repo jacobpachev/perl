ab44f1dae32738b9be3cfcdb599209a2fd7df250
Jacob Pachev <jacobpachev@gmail.com>
2016-02-19 21:08:04 -0700

    More work on files.

--- sum.pl
+++ sum.pl
@@ -7,9 +7,17 @@ print " Hello, Jacob!\n";
 my $a = "foo";
 my $b = "bar"; 
 print $a.$b."\n";
+die "Missing argument\n" unless $#ARGV >= 0;
 print "ARGV[0]=".$ARGV[0]."\n";
+
 my $fname = $ARGV[0];
 open FH, $fname or die "Could not open $fname: $!\n";
+while (my $line = <FH>)
+{
+  chop $line;
+  print "$line\n";
+}
+
 close FH;
 
 

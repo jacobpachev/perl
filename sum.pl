68535d7d0ded18ff59e8a2ae9869e512447f1e0c
Jacob Pachev <jacobpachev@gmail.com>
2016-02-24 20:02:49 -0700

    More work with files. Calculated the sum of numbers in a file.

--- sum.pl
+++ sum.pl
@@ -4,20 +4,23 @@ use strict;
 use warnings;
 
 print " Hello, Jacob!\n";
-my $a = "foo";
-my $b = "bar"; 
+my $a = "Jacob";
+my $b = "pachev"; 
 print $a.$b."\n";
 die "Missing argument\n" unless $#ARGV >= 0;
 print "ARGV[0]=".$ARGV[0]."\n";
 
 my $fname = $ARGV[0];
+print "$fname\n";
+my $s = 0;
 open FH, $fname or die "Could not open $fname: $!\n";
+
 while (my $line = <FH>)
 {
   chop $line;
-  print "$line\n";
+  $s += $line;
 }
-
+  
 close FH;
-
+print "$s\n" ;
 

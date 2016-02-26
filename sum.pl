3cc0d7019385ff9c9d0153891bebb022b7918777
Jacob Pachev <jacobpachev@gmail.com>
2016-02-25 18:51:53 -0700

    Calculated average of number's in a file.

--- sum.pl
+++ sum.pl
@@ -13,14 +13,18 @@ print "ARGV[0]=".$ARGV[0]."\n";
 my $fname = $ARGV[0];
 print "$fname\n";
 my $s = 0;
+my $n = 0;
 open FH, $fname or die "Could not open $fname: $!\n";
 
 while (my $line = <FH>)
 {
   chop $line;
   $s += $line;
+  $n++;
 }
   
 close FH;
-print "$s\n" ;
-
+print "$s\n";
+print "$n\n"; 
+my $avg = $s/$n;
+print "$avg\n";
